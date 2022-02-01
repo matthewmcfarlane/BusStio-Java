d22acfa14285c3bf35f0524ed33f96ee3f892aed
Matthew <matthewmcfarlane1@outlook.com>
2022-02-01 16:05:03 +0000

    final

---
+++ src/main/java/BusStop.java
@@ -0,0 +1,26 @@
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+
+public class BusStop {
+
+    private String name;
+    private ArrayList<Person> queue;
+
+    public BusStop(String name){
+        this.name = name;
+        this.queue = new ArrayList<Person>();
+    }
+
+    public int queueCount() {
+        return this.queue.size();
+    }
+
+    public void addToQueue(Person person) {
+        this.queue.add(person);
+    }
+
+    public Person removeFromQueue(){
+
+        return this.queue.remove(0);
+
+}}
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,56 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
+
+
+    private Bus bus;
+    private Person person;
+    private BusStop busStop;
+
+    @Before
+    public void before(){
+        bus = new Bus("Glasgow", 3);
+        person = new Person();
+        busStop = new BusStop("Argyle St.");
+        busStop.addToQueue(person);
+    }
+
+    @Test
+    public void canReturnPassengers(){
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        assertEquals(2, bus.passengerCount());
+    }
+
+    @Test
+    public void canAddPassengerBelowCapacity(){
+        bus.addPassenger(person);
+        assertEquals(1, bus.passengerCount());
+    }
+
+    @Test
+    public void cannotAddPassengerPastCapacity(){
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        assertEquals(3, bus.passengerCount());
+    }
+
+    @Test
+    public void canRemovePassenger(){
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        bus.removePassenger();
+        assertEquals(1, bus.passengerCount());
+    }
+
+    @Test
+    public void canPickUp(){
+        bus.pickUp(busStop);
+        assertEquals(1, bus.passengerCount());
+    }
 }
